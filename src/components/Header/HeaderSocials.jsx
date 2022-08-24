c6b67c37d00246e3c1cc4b44d276d8f188ca9973
SamCrdn <samuelcardon@hotmail.com>
2022-08-24 23:52:24 +0200

    loom

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import {SiLoom} from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/samuel-cardon-evenementiel-sport/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/SamCrdn" target="_blank"><FaGithub/></a>
-        <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
+        <a href="https://www.linkedin.com/in/samuel-cardon-developpeur-blockchain/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/SamCrdn" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://loom.com/invite/34b02f8f76d749b7a42d3a8e2f6f7951" target="_blank" rel="noreferrer"><SiLoom/></a>
     </div>
   )
 }
