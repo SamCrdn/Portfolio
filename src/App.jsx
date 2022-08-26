add782df75aec48ff570005298f5d91659186666
SamCrdn <samuelcardon@hotmail.com>
2022-08-26 20:49:24 +0200

    make it better

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
-import Services from './components/Services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
