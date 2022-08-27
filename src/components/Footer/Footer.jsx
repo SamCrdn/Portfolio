2c8d4abc982f2f12b4b2b2ae7de3dec58dc50dd5
SamCrdn <samuelcardon@hotmail.com>
2022-08-27 18:50:46 +0200

    testimonial

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './footer.css'
-import {FaFacebookF} from 'react-icons/fa'
-import {FiInstagram} from 'react-icons/fi'
-import {IoLogoTwitter} from 'react-icons/io'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {SiLoom} from 'react-icons/si'
 
 const Footer = () => {
   return (
@@ -19,9 +19,9 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF/></a>
-        <a href="https://instagram.com"><FiInstagram/></a>
-        <a href="https://twitter.com"><IoLogoTwitter/></a>
+        <a href="https://www.linkedin.com/in/samuel-cardon-developpeur-blockchain/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/SamCrdn" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://loom.com/invite/34b02f8f76d749b7a42d3a8e2f6f7951" target="_blank" rel="noreferrer"><SiLoom/></a>
       </div>
 
       <div className="footer__copyright">
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
+import AVTR1 from '../../assets/avatar.jpg'
+
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -19,24 +17,10 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar: AVTR1,
-    name: 'Tina Snow',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+    name: 'Antoine Monesma (3WA Academy)',
+    review: 'Despite his very slight experience Samuel shows a very good attitude and learns quickly. His motivation allows him to face problems. This state of mind will make him a good developer.'
   },
-  {
-    avatar: AVTR2,
-    name: 'Shatta Wale',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
-  },
-  {
-    avatar: AVTR3,
-    name: 'Kwame Despite',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
-  },
-  {
-    avatar: AVTR4,
-    name: 'Nana Ama McBrown',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
-  },
+
 ]
 
 
